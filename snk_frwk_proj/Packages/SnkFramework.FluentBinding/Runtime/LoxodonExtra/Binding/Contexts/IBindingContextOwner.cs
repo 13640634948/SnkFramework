416b46ed521f2e0b2308f9eeac69b5ef7227eb49
ClaineLe <7941324@qq.com>
2022-09-12 22:40:25 +0800

    [snk.fluent-binding]增加BindingContextOwner

--- snk_frwk_proj/Packages/SnkFramework.FluentBinding/Runtime/LoxodonExtra/Binding/Contexts/IBindingContextOwner.cs
+++ snk_frwk_proj/Packages/SnkFramework.FluentBinding/Runtime/LoxodonExtra/Binding/Contexts/IBindingContextOwner.cs
@@ -1,7 +1,3 @@
-using System;
-using Loxodon.Framework.Binding.Binders;
-using Loxodon.Framework.Binding.Builder;
-using Loxodon.Framework.Contexts;
 using UnityEngine;
 
 namespace Loxodon.Framework.Binding.Contexts
@@ -9,48 +5,5 @@ namespace Loxodon.Framework.Binding.Contexts
     public interface IBindingContextOwner
     {
         public IBindingContext DataContext { get; set; }
-    }
-
-    public static class BindingContextOwnerExtension
-    {
-        
-        private static IBinder binder;
-        public static IBinder Binder
-        {
-            get
-            {
-                if (binder == null)
-                    binder = Context.GetApplicationContext().GetService<IBinder>();
-
-                if (binder == null)
-                    throw new Exception("Data binding service is not initialized,please create a BindingServiceBundle service before using it.");
-
-                return binder;
-            }
-        }
-
-        public static IBindingContext BindingContext(this IBindingContextOwner bindingContextOwner)
-        {
-            if (bindingContextOwner == null)
-                return null;
-
-            IBindingContext bindingContext = bindingContextOwner.DataContext;
-            if (bindingContext == null)
-            {
-                bindingContext = new BindingContext(bindingContextOwner, Binder);
-                bindingContextOwner.DataContext = bindingContext;
-            }
-            return bindingContext;
-        }
-
-        
-        public static BindingSet<TBindingContextOwner, TSource> CreateBindingSet<TBindingContextOwner, TSource>(this TBindingContextOwner behaviour, TSource dataContext) 
-            where TBindingContextOwner : class, IBindingContextOwner
-        {
-            IBindingContext context = behaviour.BindingContext();
-            context.DataContext = dataContext;
-            return new BindingSet<TBindingContextOwner, TSource>(context, behaviour);
-        }
-
     }
 }
