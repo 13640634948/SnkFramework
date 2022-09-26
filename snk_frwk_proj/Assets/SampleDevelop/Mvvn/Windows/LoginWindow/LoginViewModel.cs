4fbea543808727cd02c24cb5cd7d1263f4d847a3
ClaineLe <7941324@qq.com>
2022-09-26 18:28:53 +0800

    [snk.mvvm]测试UGUI-Button的Command绑定

--- snk_frwk_proj/Assets/SampleDevelop/Mvvn/Windows/LoginWindow/LoginViewModel.cs
+++ snk_frwk_proj/Assets/SampleDevelop/Mvvn/Windows/LoginWindow/LoginViewModel.cs
@@ -1,3 +1,4 @@
+using Loxodon.Framework.Commands;
 using SnkFramework.Mvvm.ViewModel;
 
 namespace Windows.LoginWindow
@@ -9,6 +10,13 @@ namespace Windows.LoginWindow
         {
             get => this.tip;
             set => this.Set(ref this.tip, value, nameof(Tip));
+        }
+
+        public SimpleCommand mButtonCommand;
+
+        public LoginViewModel()
+        {
+            this.mButtonCommand = new SimpleCommand(() => log.Info("LoginViewModel - mButtonCommand"));
         }
     }
 }
--- snk_frwk_proj/Assets/SampleDevelop/Mvvn/Windows/LoginWindow/LoginWindow.cs
+++ snk_frwk_proj/Assets/SampleDevelop/Mvvn/Windows/LoginWindow/LoginWindow.cs
@@ -1,7 +1,6 @@
 using Loxodon.Framework.Binding.Builder;
 using SnkFramework.Mvvm.Base;
 using SnkFramework.Mvvm.View;
-using UnityEngine;
 using UnityEngine.UI;
 
 namespace Windows.LoginWindow
@@ -9,15 +8,18 @@ namespace Windows.LoginWindow
     public class LoginWindow : Window<LoginViewModel>
     {
         private Text mTxt;
+        public Button mButton;
 
         protected override void onInitComponents()
         {
             this.mTxt = this.mOwner.transform.Find("Text").GetComponent<Text>();
+            this.mButton = this.mOwner.transform.Find("Button").GetComponent<Button>();
         }
 
         protected override void onBindingComponents(BindingSet<View<LoginViewModel>, LoginViewModel> setter)
         {
             setter.Bind(this.mTxt).For(v => v.text).To(vm => vm.Tip);
+            setter.Bind(this.mButton).For(v => v.onClick).To(vm => vm.mButtonCommand);
         }
 
         protected override void onCreate(IBundle bundle)
