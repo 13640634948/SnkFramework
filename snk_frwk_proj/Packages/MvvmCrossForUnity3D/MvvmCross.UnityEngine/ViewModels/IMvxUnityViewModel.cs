e0e4a16bfa9063bd69dce43532f58b4a60febde6
ClaineLe <7941324@qq.com>
2022-10-17 23:21:20 +0800

    [mvx.unity3d]增加MvxUnityViewModel中间类

---
+++ snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.UnityEngine/ViewModels/IMvxUnityViewModel.cs
@@ -0,0 +1,7 @@
+namespace MvvmCross.UnityEngine.ViewModels
+{
+    public interface IMvxUnityViewModel
+    {
+        
+    }
+}
---
+++ snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.UnityEngine/ViewModels/MvxUnityViewModel.cs
@@ -0,0 +1,8 @@
+using MvvmCross.ViewModels;
+
+namespace MvvmCross.UnityEngine.ViewModels
+{
+    public class MvxUnityViewModel : MvxViewModel, IMvxUnityViewModel
+    {
+    }
+}
