9f50645965f45d677a6d06be778c1dd3a0da40cd
ClaineLe <7941324@qq.com>
2022-10-19 21:47:56 +0800

    [mvx.unity3d]拆分接口脚本

--- snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.Unity/Views/Base/MvxUnityEventSourceView.cs
+++ snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.Unity/Views/Base/MvxUnityEventSourceView.cs
@@ -3,19 +3,6 @@ using MvvmCross.Base;
 
 namespace MvvmCross.Unity.Views.Base
 {
-    public interface IMvxUnityEventSourceView : IMvxDisposeSource
-    {
-        public event EventHandler<MvxValueEventArgs<MvxUnityBundle>>? CreateCalled;
-        public event EventHandler? AppearingCalled;
-        public event EventHandler? AppearedCalled;
-        public event EventHandler<MvxValueEventArgs<bool>>? ActivateCalled;
-        public event EventHandler<MvxValueEventArgs<bool>>? PassivateCalled;
-
-        public event EventHandler? DisappearingCalled;
-        public event EventHandler? DisappearedCalled;
-        public event EventHandler? DismissCalled;
-    }
-
     public class MvxUnityEventSourceView : IMvxUnityEventSourceView
     {
         public event EventHandler<MvxValueEventArgs<MvxUnityBundle>> CreateCalled
