d3e70a5745f3abe4acb96684bf62c1cc7b5c5ebf
ClaineLe <7941324@qq.com>
2022-10-23 22:32:16 +0800

    [mvx.unity3d] MvxUGUIWindow改分布类

--- snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.Unity/Views/UGUI/IMvxUGUIWindow.cs
+++ snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.Unity/Views/UGUI/IMvxUGUIWindow.cs
@@ -6,7 +6,7 @@ namespace MvvmCross.Unity.Views.UGUI
     {
     }
 
-    public interface IMvxUGUIWindow<TViewModel> : IMvxUGUIWindow, IMvxUnityWindow<TViewModel, MvxUGUILayer>, IMvxUGUIView<TViewModel>
+    public interface IMvxUGUIWindow<TViewModel> : IMvxUGUIWindow, IMvxUnityWindow<TViewModel, IMvxUGUILayer>, IMvxUGUIView<TViewModel>
         where TViewModel : class, IMvxUnityViewModel
     {
     }
--- snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.Unity/Views/UGUI/MvxUGUIWindow.cs
+++ snk_frwk_proj/Packages/MvvmCrossForUnity3D/MvvmCross.Unity/Views/UGUI/MvxUGUIWindow.cs
@@ -1,88 +1,16 @@
-using System;
-using System.Collections;
-using MvvmCross.Base;
 using MvvmCross.Unity.ViewModels;
 
 namespace MvvmCross.Unity.Views.UGUI
 {
-    public abstract class MvxUGUIWindow<TViewModel> : MvxUGUIView<TViewModel>, IMvxUGUIWindow<TViewModel>
+    public abstract partial class MvxUGUIWindow<TViewModel> : MvxUGUIView<TViewModel>, IMvxUGUIWindow<TViewModel>
         where TViewModel : class, IMvxUnityViewModel
     {
-        protected EventHandler? showingCalled;
-        protected EventHandler<MvxValueEventArgs<bool>>? showedCalled;
-
-        protected EventHandler? hidingCalled;
-        protected EventHandler<MvxValueEventArgs<bool>>? hiddenCalled;
-        
-        private MvxUGUILayer _layer;
-        MvxUGUILayer IMvxUnityWindow<TViewModel, MvxUGUILayer>.Layer
+        IMvxUnityLayer IMvxUnityWindow.Layer
         {
-            get => _layer;
-            set => _layer = value;
-        }
-        
-        public IMvxUnityLayer Layer
-        {
-            get => _layer;
-            set => _layer = value as MvxUGUILayer;
-        }
-        
-        public event EventHandler? ShowingCalled
-        {
-            add => showingCalled += value;
-            remove => showingCalled -= value;
+            get => Layer;
+            set => Layer = value as IMvxUGUILayer;
         }
 
-        public event EventHandler<MvxValueEventArgs<bool>>? ShowedCalled
-        {
-            add => showedCalled += value;
-            remove => showedCalled -= value;
-        }
-
-        public event EventHandler? HidingCalled
-        {
-            add => hidingCalled += value;
-            remove => hidingCalled -= value;
-        }
-
-        public event EventHandler<MvxValueEventArgs<bool>>? HiddenCalled
-        {
-            add => hiddenCalled += value;
-            remove => hiddenCalled -= value;
-        }
-
-        protected virtual IEnumerator doShow()
-        {
-            yield break;
-        }
-
-        protected virtual IEnumerator doHide()
-        {
-            yield break;
-        }
-
-
-        public virtual IEnumerator Show(bool animated)
-        {
-            if (animated)
-            {
-                this.showingCalled?.Raise(this);
-                yield return doShow();
-            }
-
-            this.showedCalled?.Raise(this, animated);
-        }
-
-        public virtual IEnumerator Hide(bool animated)
-        {
-            if (animated)
-            {
-                this.hidingCalled?.Raise(this);
-                yield return doHide();
-            }
-
-            this.hiddenCalled?.Raise(this, animated);
-        }
-
+        public IMvxUGUILayer Layer { get; set; }
     }
 }
