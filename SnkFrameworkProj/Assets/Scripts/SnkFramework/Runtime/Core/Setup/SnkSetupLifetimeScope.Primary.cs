76c15ec141db751c4e3e3cfafa3b5ed5e9b4e7de
ClaineLe <7941324@qq.com>
2022-10-27 12:22:40 +0800

    [snk.framework]增加注释

--- SnkFrameworkProj/Assets/Scripts/SnkFramework/Runtime/Core/Setup/SnkSetupLifetimeScope.Primary.cs
+++ SnkFrameworkProj/Assets/Scripts/SnkFramework/Runtime/Core/Setup/SnkSetupLifetimeScope.Primary.cs
@@ -4,31 +4,67 @@ namespace SnkFramework.Runtime.Core.Setup
 {
     public partial class SnkSetupLifetimeScope
     {
+        /// <summary>
+        /// 主要初始化（UnityThread）
+        /// </summary>
+        /// <param name="builder"></param>
         public virtual void InitializePrimary(IContainerBuilder builder)
         {
             if (State != SnkSetupState.Uninitialized)
                 return;
             State = SnkSetupState.InitializingPrimary;
-            
+
             InitializeLoggingServices(builder);
-            
-            RegisterDefaultSetupDependencies();
+
+            RegisterDefaultSetupDependencies(builder);
+            InitializeBasePlatform(builder);
             InitializeFirstChance(builder);
             State = SnkSetupState.InitializedPrimary;
         }
-        
-        protected virtual void InitializeFirstChance(IContainerBuilder builder)
-        {
-        }
-
-     
+      
+        /// <summary>
+        /// 初始化日志服务
+        /// </summary>
+        /// <param name="builder"></param>
         protected virtual void InitializeLoggingServices(IContainerBuilder builder)
         {
+            
         }
 
-        protected virtual void RegisterDefaultSetupDependencies()
+        /// <summary>
+        /// 注册框架设置
+        /// </summary>
+        /// <param name="builder"></param>
+        protected virtual void RegiestFrameworkSetting(IContainerBuilder builder)
         {
+            
         }
 
+        /// <summary>
+        /// 默认的安装器依赖
+        /// </summary>
+        protected virtual void RegisterDefaultSetupDependencies(IContainerBuilder builder)
+        {
+            RegiestFrameworkSetting(builder);
+        }
+
+        /// <summary>
+        /// 初始化各个平台
+        /// </summary>
+        protected virtual void InitializeBasePlatform(IContainerBuilder builder)
+        {
+            
+        }
+
+        /// <summary>
+        /// 首初始化
+        /// </summary>
+        /// <param name="builder"></param>
+        protected virtual void InitializeFirstChance(IContainerBuilder builder)
+        {
+            // always the very first thing to get initialized
+            // base class implementation is empty by default
+            // 第一件要初始化的事，默认为空实现，需要的自行重写
+        }
     }
 }
