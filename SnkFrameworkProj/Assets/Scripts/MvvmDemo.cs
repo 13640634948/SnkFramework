56424e5c6467db88793898a34c426b3932e0543c
ClaineLe <7941324@qq.com>
2022-11-02 21:02:03 +0800

    [snk.mvvm]创建默认Window属性

--- SnkFrameworkProj/Assets/Scripts/MvvmDemo.cs
+++ SnkFrameworkProj/Assets/Scripts/MvvmDemo.cs
@@ -7,6 +7,7 @@ using SnkFramework.Mvvm.Runtime.ViewModel;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using SnkFramework.Mvvm.Runtime.Layer;
+using SnkFramework.Mvvm.Runtime.Presenters.Attributes;
 using UnityEngine.EventSystems;
 using Object = UnityEngine.Object;
 
@@ -81,6 +82,17 @@ public class DemoPresenter : SnkViewPresenter
 {
     public DemoPresenter(ISnkViewFinder viewFinder, ISnkViewLoader viewLoader, ISnkLayerContainer layerContainer) : base(viewFinder, viewLoader, layerContainer)
     {
+    }
+
+    public override SnkBasePresentationAttribute CreatePresentationAttribute(Type viewModelType, Type viewType)
+    {
+        var attribute = base.CreatePresentationAttribute(viewModelType, viewType);
+        if (attribute is SnkPresentationWindowAttribute windowAttribute)
+        {
+            windowAttribute.LayerType = typeof(SnkUGUINormalLayer);
+        }
+
+        return attribute;
     }
 }
 
--- SnkFrameworkProj/Assets/Scripts/TestWindow.cs
+++ SnkFrameworkProj/Assets/Scripts/TestWindow.cs
@@ -1,7 +1,5 @@
-using SnkFramework.Mvvm.Runtime.Presenters.Attributes;
 using SnkFramework.Mvvm.Runtime.View;
 
-[SnkPresentationWindow(LayerType = typeof(SnkUGUINormalLayer))]
 public class TestWindow : SnkWindow
 {
      
