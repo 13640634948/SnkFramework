070fd72e264ca12f0ec77b098b87bdf42e3b9bc4
ClaineLe <7941324@qq.com>
2022-11-09 14:09:06 +0800

    [snk.mvvm]RectTransform拓展方法

---
+++ SnkFrameworkProj/Packages/SnkFramework.MvvmUI/Extensions/RectTransformExtension.cs
@@ -0,0 +1,15 @@
+namespace SnkFramework.Mvvm.Extensions
+{
+    public static class RectTransformExtension
+    {
+        public static void Identity(this RectTransform rectTransform)
+        {
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+            rectTransform.anchoredPosition3D = Vector3.zero;
+            rectTransform.localScale = Vector3.one;
+        }
+    }
+}
