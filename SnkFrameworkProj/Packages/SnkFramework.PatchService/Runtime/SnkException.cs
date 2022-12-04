b137415db4d521e74d024403f142fcc745cca015
ClaineLe <7941324@qq.com>
2022-12-04 21:10:55 +0800

    [snk.patch-svr]搭建补丁服务框架

---
+++ SnkFrameworkProj/Packages/SnkFramework.PatchService/Runtime/SnkException.cs
@@ -0,0 +1,10 @@
+namespace SnkFramework.PatchService.Runtime
+{
+    public class SnkException : System.Exception
+    {
+        public SnkException(string message) : base(message)
+        {
+            
+        }
+    }
+}
