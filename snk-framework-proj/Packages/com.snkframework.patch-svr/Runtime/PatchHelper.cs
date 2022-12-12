f0faa15cbd1f02462367977f0e976b215c6dcc27
ClaineLe <7941324@qq.com>
2022-12-12 19:07:30 +0800

    [snk.patch-svr]增加文件生成MD5静态方法

--- snk-framework-proj/Packages/com.snkframework.patch-svr/Runtime/PatchHelper.cs
+++ snk-framework-proj/Packages/com.snkframework.patch-svr/Runtime/PatchHelper.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using SnkFramework.PatchService.Runtime.Core;
 
 namespace SnkFramework.PatchService.Runtime
@@ -53,6 +55,49 @@ namespace SnkFramework.PatchService.Runtime
                 fromFileInfo.CopyTo(toFileInfo.FullName);
             }
         }
-
+        
+        /// <summary>
+        /// 文件生成MD5（适用小文件）
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string getMD5ByMD5CryptoService(string path)        
+        {            
+            if (!File.Exists(path)) return "";            
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);            
+            MD5CryptoServiceProvider md5Provider = new MD5CryptoServiceProvider();            
+            byte[] buffer = md5Provider.ComputeHash(fs);            
+            string resule = BitConverter.ToString(buffer);           
+            md5Provider.Clear();            
+            fs.Close();           
+            return resule;        
+        }
+        
+        /// <summary>
+        /// 文件生成MD5（适用大文件）
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string getMD5ByHashAlgorithm(string path)        
+        {            
+            if (!File.Exists(path)) return "";            
+            int bufferSize = 1024 * 16;//自定义缓冲区大小16K            
+            byte[] buffer = new byte[bufferSize];            
+            Stream inputStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);           
+            HashAlgorithm hashAlgorithm = new MD5CryptoServiceProvider();            
+            int readLength = 0;//每次读取长度            
+            var output = new byte[bufferSize];            
+            while ((readLength = inputStream.Read(buffer, 0, buffer.Length)) > 0)            
+            {               
+                //计算MD5                
+                hashAlgorithm.TransformBlock(buffer, 0, readLength, output, 0);            
+            }            
+            //完成最后计算，必须调用(由于上一部循环已经完成所有运算，所以调用此方法时后面的两个参数都为0)            		  
+            hashAlgorithm.TransformFinalBlock(buffer, 0, 0);            
+            string md5 = BitConverter.ToString(hashAlgorithm.Hash);                        
+            hashAlgorithm.Clear();            
+            inputStream.Close();            
+            return md5;        
+        }
     }
 }
