df5d526568428f7de3c0904c7ee4daf2204dba90
ClaineLe <7941324@qq.com>
2023-01-29 19:36:55 +0800

    [nuget.basic]新增单例基类

---
+++ snk.framework.nuget/scr/basic/SnkSingleton.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using SnkFramework.NuGet.Exceptions;
+
+namespace SnkFramework.NuGet
+{
+    namespace Basic
+    {
+        public abstract class SnkSingleton : IDisposable
+        {
+            ~SnkSingleton()
+            {
+                Dispose(false);
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected abstract void Dispose(bool isDisposing);
+
+            private static readonly List<SnkSingleton> Singletons = new List<SnkSingleton>();
+
+            protected SnkSingleton()
+            {
+                lock (Singletons)
+                {
+                    Singletons.Add(this);
+                }
+            }
+
+            public static void ClearAllSingletons()
+            {
+                lock (Singletons)
+                {
+                    foreach (var s in Singletons)
+                    {
+                        s.Dispose();
+                    }
+
+                    Singletons.Clear();
+                }
+            }
+        }
+
+        public abstract class SnkSingleton<TInterface> : SnkSingleton where TInterface : class
+        {
+            protected SnkSingleton()
+            {
+                if (Instance != null)
+                    throw new SnkException("You cannot create more than one instance of SnkSingleton");
+
+                Instance = this as TInterface;
+            }
+
+            public static TInterface Instance { get; private set; }
+
+            protected override void Dispose(bool isDisposing)
+            {
+                if (isDisposing)
+                {
+                    Instance = null;
+                }
+            }
+        }
+    }
+}
