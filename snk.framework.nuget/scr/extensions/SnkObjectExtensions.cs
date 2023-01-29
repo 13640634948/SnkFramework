c2c490492d2501fb3e8e69a45937b96356fde804
ClaineLe <7941324@qq.com>
2023-01-29 18:40:03 +0800

    [extensions]新增Object的拓展类

---
+++ snk.framework.nuget/scr/extensions/SnkObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnkFramework.NuGet
+{
+    namespace Extensions
+    {
+        public static class SnkObjectExtensions
+        {
+            public static void DisposeIfDisposable(this object thing)
+            {
+                if (thing is IDisposable disposable)
+                    disposable.Dispose();
+            }
+        }
+    }
+}
+
+
+
