63741f1a6a9f8b0a82887a6ae60c91ffcb8e7419
ClaineLe <7941324@qq.com>
2023-01-30 10:55:44 +0800

    [nuget.internal]新增Internal目录，添加LogHost

---
+++ snk.framework.nuget/scr/internal/SnkLogHost.cs
@@ -0,0 +1,36 @@
+﻿using SnkFramework.NuGet.Basic;
+
+namespace SnkFramework.NuGet
+{
+    internal class SnkLogHost
+    {
+        /*
+        private static ILogger? _defaultLogger;
+
+        public static ILogger? Default => _defaultLogger ??= GetLog("Default");
+
+        public static ILogger<T>? GetLog<T>() =>
+            Mvx.IoCProvider?.TryResolve<ILoggerFactory>(out var loggerFactory) == true
+                ? loggerFactory.CreateLogger<T>()
+                : null;
+
+        public static ILogger? GetLog(string categoryName) =>
+            Mvx.IoCProvider?.TryResolve<ILoggerFactory>(out var loggerFactory) == true
+                ? loggerFactory.CreateLogger(categoryName)
+                : null;
+        */
+
+        private static ISnkLogger _defaultLogger;
+
+        public static ISnkLogger Default
+        {
+            get
+            {
+                if( _defaultLogger == null)
+                    _defaultLogger = Snk.Get<ISnkLogger>();
+                return _defaultLogger;
+            }
+        }
+    }
+}
+
