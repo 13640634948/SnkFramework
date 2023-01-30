d0960a29e351151a342ee1fb144a4f6d6c6009df
ClaineLe <7941324@qq.com>
2023-01-30 10:55:01 +0800

    [nuget.basic]拓展目录迁移至Basic目录下，并合并为一个脚本，后续根据需求再拆分开

---
+++ snk.framework.nuget/scr/basic/SnkExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SnkFramework.NuGet
+{
+    namespace Basic
+    {
+        public static class SnkObjectExtensions
+        {
+            public static void DisposeIfDisposable(this object thing)
+            {
+                if (thing is IDisposable disposable)
+                    disposable.Dispose();
+            }
+        }
+
+        public static class MvxDictionaryExtensions
+        {
+            public static IDictionary<string, object> ToPropertyDictionary(this object input)
+            {
+                if (input == null)
+                    return new Dictionary<string, object>();
+
+                if (input is IDictionary<string, object> dict)
+                    return dict;
+
+                var propertyInfos =
+                    input.GetType().GetProperties(
+                        BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy)
+                    .Where(p => p.CanRead);
+
+                var dictionary = new Dictionary<string, object>();
+                foreach (var propertyInfo in propertyInfos)
+                {
+                    dictionary[propertyInfo.Name] = propertyInfo.GetValue(input);
+                }
+                return dictionary;
+            }
+        }
+    }
+}
+
+
+
