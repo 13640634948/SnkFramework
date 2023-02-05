9b86c323283fbdd9a7660f7ddb425aa7fdccbf25
ClaineLe <7941324@qq.com>
2023-02-05 14:49:04 +0800

    [upm.mvvm-ui]实现GetViewType接口

--- snk-framework-proj/Packages/snk.framework.mvvm-ui/Runtime/View/SnkViewLoader.cs
+++ snk-framework-proj/Packages/snk.framework.mvvm-ui/Runtime/View/SnkViewLoader.cs
@@ -1,17 +1,70 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using SnkFramework.Mvvm.Runtime.Presenters;
 using SnkFramework.Mvvm.Runtime.ViewModel;
+using UnityEngine;
 
 namespace SnkFramework.Mvvm.Runtime
 {
     namespace View
     {
-        public abstract class SnkViewLoader : ISnkViewLoader
+        public interface IMvxViewsContainer : ISnkViewFinder
         {
+            void AddAll(IDictionary<Type, Type> viewModelViewLookup);
+
+            void Add(Type viewModelType, Type viewType);
+
+            void Add<TViewModel, TView>()
+                where TViewModel : ISnkViewModel
+                where TView : ISnkView;
+
+            void AddSecondary(ISnkViewFinder finder);
+
+            void SetLastResort(ISnkViewFinder finder);
+        }
+        
+        public abstract class SnkViewLoader : IMvxViewsContainer, ISnkViewLoader
+        {
+            private readonly Dictionary<Type, Type> _bindingMap = new Dictionary<Type, Type>();
+            private readonly List<ISnkViewFinder> _secondaryViewFinders;
+            private ISnkViewFinder? _lastResortViewFinder;
+            
+            public void Add(Type viewModelType, Type viewType)
+            {
+                Debug.Log("SnkViewLoader-Add:" + viewModelType + ", " + viewType);
+                _bindingMap[viewModelType] = viewType;
+            }
+
+            public void Add<TViewModel, TView>()
+                where TViewModel : ISnkViewModel
+                where TView : ISnkView
+            {
+                Add(typeof(TViewModel), typeof(TView));
+            }
+
+            public void AddSecondary(ISnkViewFinder finder)
+            {
+                _secondaryViewFinders.Add(finder);
+            }
+
+            public void SetLastResort(ISnkViewFinder finder)
+            {
+                _lastResortViewFinder = finder;
+            }
+
+            public void AddAll(IDictionary<Type, Type> viewModelViewLookup)
+            {
+                foreach (var pair in viewModelViewLookup)
+                {
+                    Add(pair.Key, pair.Value);
+                }
+            }
+
             public virtual async Task<SnkWindow> CreateView(SnkViewModelRequest request)
             {
                 var viewType = GetViewType(request.ViewModelType);
+                Debug.Log("CreateView-viewType:" + viewType);
                 return await CreateView(viewType);
             }
 
@@ -23,7 +76,34 @@ namespace SnkFramework.Mvvm.Runtime
                 return true;
             }
 
-            public abstract Type GetViewType(Type viewModelType);
+            public virtual Type GetViewType(Type viewModelType)
+            {
+                Type? binding;
+                if (viewModelType != null && _bindingMap.TryGetValue(viewModelType, out binding))
+                {
+                    return binding;
+                }
+
+                foreach (var viewFinder in _secondaryViewFinders)
+                {
+                    binding = viewFinder.GetViewType(viewModelType);
+                    if (binding != null)
+                    {
+                        return binding;
+                    }
+                }
+
+                if (_lastResortViewFinder != null)
+                {
+                    binding = _lastResortViewFinder.GetViewType(viewModelType);
+                    if (binding != null)
+                    {
+                        return binding;
+                    }
+                }
+
+                throw new KeyNotFoundException("Could not find view for " + viewModelType);
+            }
 
             public virtual Type GetViewType<TViewModel>() where TViewModel : class, ISnkViewModel
                 => GetViewType(typeof(TViewModel));
