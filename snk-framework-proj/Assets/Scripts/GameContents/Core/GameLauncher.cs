e902f7658512a85093708e0026993cfb41088137
Claine <7941324@qq.com>
2023-02-08 17:07:35 +0800

    [unity.game]闪屏界面

--- snk-framework-proj/Assets/Scripts/GameContents/Core/GameLauncher.cs
+++ snk-framework-proj/Assets/Scripts/GameContents/Core/GameLauncher.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using GAME.Contents.UserInterfaces;
+using SnkFramework.Runtime;
 using SnkFramework.Runtime.Core;
 using SnkFramework.Runtime.Engine;
 using UnityEngine;
@@ -11,20 +14,14 @@ namespace GAME.Contents.Core
 {
     public class GameLauncher : MonoBehaviour, ISnkSetupMonitor
     {
+        private SplashScreen _splashScreen;
+        
         private static string[] namespaces =
         {
             "SnkFramework.Runtime",
             "BFFramework.Runtime",
             "Game.Contexts.",
         };
-        
-        public async void Start()
-        {
-            var assemblies = GetGameAssembly();
-            var instance = await SnkUnitySetupSingleton.EnsureSingletonAvailable<GameSetup>(assemblies.ToArray());
-            await instance.EnsureInitialized(this);
-            instance.AsyncRunAppStart();
-        }
 
         private static IEnumerable<Assembly> GetGameAssembly()
         {
@@ -34,9 +31,31 @@ namespace GAME.Contents.Core
                 select assembly;
         }
 
+        private async Task<SplashScreen> LoadSplashScreen()
+        {
+            var asset = await Resources.LoadAsync<GameObject>("SplashScreen/SplashScreen") as GameObject;
+            return Instantiate(asset).GetComponent<SplashScreen>();
+        }
+
+        public async void Start()
+        {
+            this._splashScreen = await LoadSplashScreen();
+            this._splashScreen.Play();
+            
+            await UniTask.WaitUntil(() => this._splashScreen.mPrepareCompleted);
+            var assemblies = GetGameAssembly().ToArray();
+            var instance = await SnkUnitySetupSingleton.EnsureSingletonAvailable<GameSetup>(assemblies);
+            await UniTask.WaitUntil(() => this._splashScreen.mPrepareCompleted);
+            await instance.EnsureInitialized(this);
+        }
+
         public async Task InitializationComplete()
         {
-            //await UniTask.WaitUntil(() => finish);
+            await Snk.IoCProvider.Resolve<ISnkAppStart>()?.StartAsync();
+            /** 这行代码可以考虑在编辑器中屏蔽，就可以跳过闪屏界面 */
+            await UniTask.WaitUntil(() => this._splashScreen.mFinish);
+            _splashScreen.Dispose(1.5f);
+            Destroy(this.gameObject);
         }
     }
 }
---
+++ snk-framework-proj/Assets/Scripts/GameContents/UserInterfaces/SplashScreen.cs
@@ -0,0 +1,141 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+namespace GAME.Contents.UserInterfaces
+{
+    public class SplashScreen : MonoBehaviour
+    {
+        private const int DISCARD_START_FRAME_NUM = 0;
+
+        public VideoPlayer mVideoPlayer;
+
+        public RawImage mRawImage;
+        public virtual bool mFinish { get; protected set; } = false;
+
+        protected bool _prepareCompleted = false;
+        public virtual bool mPrepareCompleted => _prepareCompleted && this.mVideoPlayer.frame >= DISCARD_START_FRAME_NUM;
+
+        private bool destroying = false;
+        
+        private float _fadeOutTime = 0;
+        private float currFadeOutTime = 0;
+        private float progress = 0;
+        private Color lerpColor = Color.white;
+        
+        private void Awake()
+        {
+            mVideoPlayer.errorReceived += onErrorReceived;
+            mVideoPlayer.frameDropped += onFrameDropped;
+            mVideoPlayer.frameReady += onFrameReady;
+            mVideoPlayer.loopPointReached += onLoopPointReached;
+            mVideoPlayer.prepareCompleted += onPrepareCompleted;
+            mVideoPlayer.seekCompleted += onSeekCompleted;
+            mVideoPlayer.started += onStarted;
+        }
+
+        void Start()
+        {
+            mVideoPlayer.targetTexture = new RenderTexture((int)mVideoPlayer.clip.width,(int)mVideoPlayer.clip.height, 32);
+            mRawImage.texture = mVideoPlayer.targetTexture;
+            mRawImage.color = Color.white;
+        }
+
+        
+        private void Update()
+        {
+            if (destroying == false)
+                return;
+            
+            if (progress >= 1.0f)
+            {
+                destroying = false;
+                mVideoPlayer.targetTexture.Release();
+                Destroy(this.gameObject);                
+            }
+
+            if (currFadeOutTime < _fadeOutTime)
+                currFadeOutTime += Time.unscaledDeltaTime;
+
+            progress = currFadeOutTime / _fadeOutTime;
+            if (progress > 1.0f)
+            {
+                progress = 1.0f;
+            }
+
+            lerpColor.a = 1-progress;
+            this.mRawImage.color = lerpColor;
+        }
+
+        public void Play()
+        {
+            mVideoPlayer.Play();
+        }
+
+        /// <summary>
+        /// 错误监听到时被执行
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="message"></param>
+        protected virtual void onErrorReceived(VideoPlayer source, string message)
+        {
+            
+        }
+
+        /// <summary>
+        /// 有丢帧发生时被执行
+        /// </summary>
+        /// <param name="source"></param>
+        protected virtual void onFrameDropped(VideoPlayer source)
+        {
+            
+        }
+        
+        /// <summary>
+        /// 新的一帧准备好时被执行
+        /// </summary>
+        protected virtual void onFrameReady(VideoPlayer source, long frameIdx)
+        {
+            Debug.Log("frameIdx:" + frameIdx);
+        }
+
+        /// <summary>
+        /// 播放结束或播放到循环的点时被执行
+        /// </summary>
+        protected virtual void onLoopPointReached(VideoPlayer source)
+        {
+            this.mFinish = true;
+        }
+
+        /// <summary>
+        /// 视频准备完成时被执行
+        /// </summary>
+        protected virtual void onPrepareCompleted(VideoPlayer source)
+        {
+            Debug.Log("onPrepareCompleted");
+            this._prepareCompleted = true;
+        }
+
+        /// <summary>
+        /// 查询帧操作完成时被执行
+        /// </summary>
+        protected virtual void onSeekCompleted(VideoPlayer source)
+        {
+        }
+
+        /// <summary>
+        /// 在Play方法调用之后立刻调用
+        /// </summary>
+        protected virtual void onStarted(VideoPlayer source)
+        {
+        }
+
+        public void Dispose(float fadeOutTime)
+        {
+            if(destroying == true)
+                return;
+            this._fadeOutTime = fadeOutTime;
+            this.destroying = true;
+        }
+    }
+}
