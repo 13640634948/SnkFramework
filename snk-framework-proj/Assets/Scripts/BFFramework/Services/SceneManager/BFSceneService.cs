634963f7b408d485f9052de0971856680f4dd761
Claine <7941324@qq.com>
2023-02-10 12:14:57 +0800

    [unity.snk.main]实现场景服务

--- snk-framework-proj/Assets/Scripts/BFFramework/Services/SceneManager/BFSceneService.cs
+++ snk-framework-proj/Assets/Scripts/BFFramework/Services/SceneManager/BFSceneService.cs
@@ -1,7 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 namespace BFFramework.Runtime.Services
 {
     public class BFSceneService : BFServiceBase, IBFSceneService
     {
-        
+        public AsyncOperation LoadSceneAsync(string sceneName, bool additive)
+        {
+            var mode = additive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+            var operation = SceneManager.LoadSceneAsync(sceneName, mode);
+            return operation;
+        }
+
+        public AsyncOperation UnloadCurrScene()
+        {
+            var currScene = SceneManager.GetActiveScene();
+            return UnloadSceneAsync(currScene.name);
+        }
+
+        public AsyncOperation UnloadSceneAsync(string sceneName)
+        {
+            return SceneManager.UnloadSceneAsync(sceneName);
+        }
     }
 }
