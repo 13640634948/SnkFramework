b0e198d61650c6eac6aadebfb99c812fcc35be45
Claine <7941324@qq.com>
2023-02-10 21:27:21 +0800

    [unity.game]清理界面冗余代码

--- snk-framework-proj/Assets/Scripts/GameContents/UserInterfaces/Patch/PatchWindow.cs
+++ snk-framework-proj/Assets/Scripts/GameContents/UserInterfaces/Patch/PatchWindow.cs
@@ -1,8 +1,6 @@
 using BFFramework.Runtime.UserInterface;
 using GAME.Contents.UserInterfaces.ViewModels;
 using Loxodon.Framework.Binding;
-using Loxodon.Framework.Binding.Builder;
-using Loxodon.Framework.Binding.Contexts;
 using SnkFramework.Mvvm.Runtime.Base;
 using UnityEngine;
 using UnityEngine.UI;
@@ -18,18 +16,18 @@ namespace GAME.Contents.UserInterfaces
             private bool enable;
 
             public bool Enable {
-                get{ return this.enable; }
-                set{ this.Set<bool> (ref this.enable, value); }
+                get => this.enable;
+                set => this.Set<bool> (ref this.enable, value);
             }
 
             public float Progress {
-                get{ return this.progress; }
-                set{ this.Set<float> (ref this.progress, value); }
+                get => this.progress;
+                set => this.Set<float> (ref this.progress, value);
             }
 
             public string Tip {
-                get{ return this.tip; }
-                set{ this.Set<string> (ref this.tip, value); }
+                get => this.tip;
+                set => this.Set<string> (ref this.tip, value);
             }
             
             public override void Prepare(ISnkBundle parameterBundle)
@@ -43,25 +41,18 @@ namespace GAME.Contents.UserInterfaces
     {
         public class PatchWindow : BFWindow
         {
-            public string xxoo = "";
-            
             public Slider downloadSlider;
             public Text progressText;
             public Text tipText;
 
-            private PatchViewModel _viewModel;
             protected override void onCreate(ISnkBundle bundle = null)
             {
                 base.onCreate(bundle);
-                _viewModel = new PatchViewModel();
-                var bindingSet = this.CreateBindingSet(_viewModel);
+                var viewModel = new PatchViewModel();
+                var bindingSet = this.CreateBindingSet(viewModel);
+                
                 bindingSet.Bind(this.tipText).For(v => v.text).To(vm => vm.Tip);
                 bindingSet.Build();
-            }
-
-            private void Update()
-            {
-                _viewModel.Tip = xxoo;
             }
         }
     }
