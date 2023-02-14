5fbf0047fcc2760bc5aae4308947bea4309ad171
Claine <7941324@qq.com>
2023-02-14 12:40:16 +0800

    [nuget.http-web][review]错误码抽离新的cs文件

---
+++ snk.framework.nuget/scr/features/httpweb/SNK_HTTP_ERROR_CODE.cs
@@ -0,0 +1,41 @@
+﻿namespace SnkFramework.NuGet.Features
+{
+    namespace HttpWeb
+    {
+        /// <summary>
+        /// 错误码
+        /// </summary>
+        public enum SNK_HTTP_ERROR_CODE
+        {
+            /// <summary>
+            /// 成功
+            /// </summary>
+            succeed = 0,
+
+            /// <summary>
+            /// 错误
+            /// </summary>
+            error = 1,
+
+            /// <summary>
+            /// 无法找到文件长度
+            /// </summary>
+            can_not_find_length = 2,
+
+            /// <summary>
+            /// 下载异常
+            /// </summary>
+            download_error = 3,
+
+            /// <summary>
+            /// 文件错误
+            /// </summary>
+            file_error = 4,
+
+            /// <summary>
+            /// 目标文件大小为0
+            /// </summary>
+            target_file_size_is_zero = 5,
+        }
+    }
+}
