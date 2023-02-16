40c84d680bdbe846d1d86c707afee1da1a0c8666
ClaineLe <7941324@qq.com>
2023-02-16 12:52:36 +0800

    [nuget.logging]优化Log接口

--- snk.framework.nuget/scr/features/logging/ISnkLogger.cs
+++ snk.framework.nuget/scr/features/logging/ISnkLogger.cs
@@ -7,17 +7,19 @@
             string CategoryName { get; }
             bool Enable { get; set; }
 
+            bool IsWarnEnabled { get; }
+
             void Debug(string message);
-            void Debug(string formater, params object[] args);
+            void DebugFormat(string formater, params object[] args);
 
             void Warning(string message);
-            void Warning(string formater, params object[] args);
+            void WarnFormat(string formater, params object[] args);
 
             void Info(string message);
-            void Info(string formater, params object[] args);
+            void InfoFormat(string formater, params object[] args);
 
             void Error(string message);
-            void Error(string formater, params object[] args);
+            void ErrorFormat(string formater, params object[] args);
 
             void Exception(System.Exception e, string format, params object[] args);
         }
