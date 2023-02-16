1c47ce0209b2a7844b15f62decc139c2d589eae4
ClaineLe <7941324@qq.com>
2023-02-16 15:15:14 +0800

    [nuget.async][fixed]错误的接口继承关系

--- snk.framework.nuget/scr/asynchronous/interfaces/ISnkCallbackable.cs
+++ snk.framework.nuget/scr/asynchronous/interfaces/ISnkCallbackable.cs
@@ -6,12 +6,12 @@ namespace SnkFramework.NuGet
     {
         public interface ISnkCallbackable
         {
-
+            void OnCallback(Action<ISnkAsyncResult> callback);
         }
 
-        public interface ISnkCallbackable<TResult> : ISnkCallbackable
+        public interface ISnkCallbackable<TResult>
         {
-
+            void OnCallback(Action<ISnkAsyncResult<TResult>> callback);
         }
     }
 }
