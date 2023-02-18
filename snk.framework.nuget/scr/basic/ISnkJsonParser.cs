9b53e1c800528b2bb8962aefbf918575c09af0fb
ClaineLe <7941324@qq.com>
2023-02-18 20:02:22 +0800

    [nuget.base]JsonParse增加类型对象接口

--- snk.framework.nuget/scr/basic/ISnkJsonParser.cs
+++ snk.framework.nuget/scr/basic/ISnkJsonParser.cs
@@ -9,7 +9,7 @@
             /// </summary>
             /// <param name="target">序列化对象</param>
             /// <returns>Json字符串</returns>
-              string ToJson(object target);
+            string ToJson(object target);
 
             /// <summary>
             /// 从Json字符串构建出对象
@@ -17,7 +17,15 @@
             /// <param name="json">json字符串</param>
             /// <typeparam name="T">需要构建的对象</typeparam>
             /// <returns>反序列化对象</returns>
-              T FromJson<T>(string json) where T : class;
+            T FromJson<T>(string json) where T : class;
+
+            /// <summary>
+            /// 从Json字符串构建出对象
+            /// </summary>
+            /// <param name="json"></param>
+            /// <param name="objType"></param>
+            /// <returns></returns>
+            object FromJson(string json, System.Type objType);
         }
     }
 }
